dc4933770c217046a8696b4d2770e7eff1b576c0
Jorge Falcon <jfalcon95@outlook.com>
2015-11-08 06:24:51 -0600

    Add vendo parse

---
+++ js/vendoParse.js
@@ -0,0 +1,28 @@
+Parse.initialize("I7CHTBcgr31hehQp0QFRothSxnW5Dsa8W8GoUEzQ", "NO8ucTyA8ow5z6VejHd5u1PgABWPBBgdXKNWlfLS");
+
+var ParseObj = Parse.Object.extend("Pedidos");
+
+function publicar(){
+	var date = new Date(day, month, year);
+
+	var data = {
+		nombre		: $("vendonombre").val(),
+		descripcion : $("vendoDescripcion").val(),
+		categoria 	: $("vendoCategoria").val(),
+		fecha	  	: date;
+	};
+
+	parseObj = new ParseObj();
+
+	$("#vendoForm")[0].reset();
+
+	parseObj.save(data,{
+		success: function(){
+			console.log("info saved");
+		},
+		error: function(){
+			console.log("info failed");
+			alert("Erro: " + error.message);
+		}
+	})
+}
